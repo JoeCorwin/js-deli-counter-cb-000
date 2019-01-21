3f1bf26d36766861f1995b681a2bf98108739e19
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 21:27:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function takeANumber(line, custName) {
     line[n] = custName;
     return `Welcome, ${line[n]}. You are number ${n + 1} in line.`;
   }
+
+function nowServing(katzDeliLine) {
+   var n = katzDeliLine.length;
+    if (n === 0) {
+      return "There is nobody waiting to be served!"
+    }
+    else {
+      return `${katzDeliLine[0]}`
+    }
+  
+}
